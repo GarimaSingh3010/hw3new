b0facedb77c8edf32a88a33eff71adb0d0f81492
Garima Singh <garima.singh@kellogg.northwestern.edu>
2023-05-22 04:38:03 +0000

    aa

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,7 +11,9 @@ def create
   @post["posted_on"]=params["post"]["posted_on"]
   @post["place_id"]=params["post"]["place_id"]
   @post.save
-  redirect_to"/places/#{@post["place_id"]}
+  redirect_to"/places/#{@post["place_id"]}"
+end
 end
 
-end
+
+
