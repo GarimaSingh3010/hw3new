152f2dc90bc7f49862473bebdfc1015214206f6f
Garima Singh <garima.singh@kellogg.northwestern.edu>
2023-05-22 04:21:40 +0000

    mm

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,28 +1,5 @@
 class Place < ApplicationRecord
   # talks to the "places" table
 
-def index
-  @places=Place.all
-
-end
-
-def new
-  @place=Place.new
-end
-
-def create
-  @place= Place.new
-  @Place["name"]=params["place"]["name"]
-  @place.save
-  redirect_to"/places"
-  
-end
-
-def show
-  @place=Place.find_by({"id"=>params["id"]})
-  @posts=Post.where({"place_id"=>@place["id"]})
-
-end
-
 end
 
